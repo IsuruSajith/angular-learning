bca9fb7c3a2ac1e49daa311be03c61d5cc66993c
Isuru <isurusajith0@gmail.com>
2023-09-13 10:28:01 +0530

    add and complete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MainComponent} from "./main/main.component";
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-
-
+    AppComponent,MainComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
