a2ce9006acb535eaadc4d36d395b3d8e6d249d23
Isuru <isurusajith0@gmail.com>
2023-09-15 13:09:28 +0530

    going to create header with using separate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import {MainComponent} from "./main/main.component";
 import { NavigationComponent } from './navigation/navigation.component';
 import { ContentComponent } from './content/content.component';
 import { TestComponent } from './test/test.component';
+import { HeaderComponent } from './header/header.component';
+import { TopMenuComponent } from './header/top-menu/top-menu.component';
+import { MainMenuComponent } from './header/main-menu/main-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,MainComponent, NavigationComponent, ContentComponent, TestComponent
+    AppComponent,MainComponent, NavigationComponent, ContentComponent, TestComponent, HeaderComponent, TopMenuComponent, MainMenuComponent
   ],
   imports: [
     BrowserModule
