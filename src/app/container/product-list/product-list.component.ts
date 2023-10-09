a10eedab97e1ea6c7ab7a00d6302054c8237c693
Isuru <isurusajith0@gmail.com>
2023-10-09 08:49:28 +0530

    create an product object

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -8,6 +8,17 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
 product = [{
  id : 1,
-  name:"nike running shoe"
+  name:"nike running shoe",
+  description:"thoga seeminthai",
+  brand:"nike",
+  gender:"men",
+  category: "running",
+  size: [38, 39, 40, 41, 42],
+  color:["white","black","green","blue"],
+  price:160,
+  is_in_inventory:true,
+  items_left:3,
+  imageURL: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Ftrainers-shoes&psig=AOvVaw3a1C5p1B3Jrc7FLXN4UkRw&ust=1696907818273000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCIiFrK-A6IEDFQAAAAAdAAAAABAE",
+
 }]
 }
