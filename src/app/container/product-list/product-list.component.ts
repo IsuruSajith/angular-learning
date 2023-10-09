266e621c2eda8425b36c4351440eb7ce62304add
Isuru <isurusajith0@gmail.com>
2023-10-09 10:06:32 +0530

    add another object

--- src/app/container/product-list/product-list.component.ts
+++ src/app/container/product-list/product-list.component.ts
@@ -20,5 +20,23 @@ product = [{
   items_left:3,
   imageURL: "https://media.istockphoto.com/id/1350560575/photo/pair-of-blue-running-sneakers-on-white-background-isolated.jpg?s=612x612&w=0&k=20&c=A3w_a9q3Gz-tWkQL6K00xu7UHdN5LLZefzPDp-wNkSU=",
 
-}]
+},
+
+  {
+    id : 2,
+    name:"nike running shoe",
+    description:"thoga seeminthai",
+    brand:"nike",
+    gender:"men",
+    category: "running",
+    size: [38, 39, 40, 41, 42],
+    color:["white","black","green","blue"],
+    price:160,
+    is_in_inventory:true,
+    items_left:3,
+    imageURL: "https://img.freepik.com/free-photo/pair-trainers_144627-3800.jpg",
+
+  }
+
+]
 }
